01a26655a05b294a0d11d1bb5d7a9f59789b0587
hafiz <hafizulrahim@yahoo.com>
2024-01-14 22:19:35 +0800

    Initial commit

---
+++ src/main/java/com/ms/lesson/SpringWebv1/SpringWebv1Application.java
@@ -0,0 +1,13 @@
+package com.ms.lesson.SpringWebv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/lesson/SpringWebv1/SpringWebv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.lesson.SpringWebv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
